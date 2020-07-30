7497565a6b9dd6803f915e9c79f978be5f62496d
Edgar Herrera <edgarinelfuerte@hotmail.com>
2020-07-30 03:06:46 -0500

    ?

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once realpath('vendor/autoload.php');
+require_once realpath('../vendor/autoload.php');
 
 echo '1';
 
