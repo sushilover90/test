ad7419e04d91c4b755fb6a1da05d4eb990a187cd
Edgar Herrera <edgarinelfuerte@hotmail.com>
2020-07-30 02:35:34 -0500

    First commit

---
+++ src/Clases/Conexiones/ConexionMariaDB.php
@@ -0,0 +1,9 @@
+<?
+
+namespace Integradora\Clases\Conexiones;
+
+class ConexionMariaDB {
+	public function __construct(){
+		echo 'Connected to MariaDB.';
+	}
+}
---
+++ src/Clases/Datos/Playera.php
@@ -0,0 +1,10 @@
+<?
+
+namespace Integradora\Clases\Datos;
+
+class Playera {
+	public function __construct(){
+		echo 'Made a playera.';
+	}
+}
+
